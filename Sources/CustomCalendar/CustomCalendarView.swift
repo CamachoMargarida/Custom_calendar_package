540e517c44a1b4ccbcdc20c2047f183d9d1248c2
Margarida Camacho <margarida.camacho@axians.com>
2024-10-24 18:58:01 +0100

    Add example to print message.

---
+++ Sources/CustomCalendar/CustomCalendarView.swift
@@ -0,0 +1,12 @@
+//
+//  CustomCalendarView.swift
+//  CustomCalendar
+//
+//  Created by Margarida Camacho on 24/10/2024.
+//
+
+public class CustomCalendarView {
+    public static func example(message: String) {
+        print("Test: \(message)")
+    }
+}
